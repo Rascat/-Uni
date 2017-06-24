986d8bb229e2c14377728d0188868b2314087b43
Rascat <ls80zyse@studserv.uni-leipzig.de>
2017-06-24 18:00:06 +0200

    write dictDB.py

---
+++ PYTHON_Code/ueb6/dictDB.py
@@ -0,0 +1,40 @@
+dictDB = {}
+fileName = "dictionaryDB.txt"
+
+def addUser(dictDB, string):
+    data = string.split(";")
+    newEntry = {'forename':data[1], 'surname':data[2], 'job':data[3], 'age':int(data[4])}
+    dictDB[int(data[0])] = newEntry
+
+def readFile(dictDB, fileName):
+    f = open(fileName, 'r')
+
+    for line in f:
+        addUser(dictDB, line)
+
+def removeUser(dictDB, id):
+    
+    if id in dictDB:
+        del dictDB[id]
+    else:
+        print("The Key is not in the Dictionary")
+
+def printUser(dictDB, id):
+    
+    if id in dictDB:
+        user = dictDB[id]
+        print("Vorname: %s, Nachname: %s" % (user['forename'], user['surname']))
+    else:
+        print("the key is not in the Dictionary")
+
+readFile(dictDB,fileName)
+for id in dictDB:
+    user = dictDB[id]
+    if user['job'] == 'Mathematiker':
+        removeUser(dictDB, id)
+for id in dictDB:
+    user = dictDB[id]
+    if user['age'] == 23:
+        printUser(dictDB, id)
+    
+
